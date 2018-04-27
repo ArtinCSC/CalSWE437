bb49deced38fa06a3e8bee35b66960a6500aa72b
Artin <am25938@email.vccs.edu>
2018-04-26 20:43:31 -0400

    k

--- CACCtest.java
+++ CACCtest.java
@@ -11,9 +11,15 @@ public class CACCtest {
 	}
 	
 	@Test
-	public void test() {
+	public void testP1() {
 		int month1 = 4, month2 = 4, day1 = 1, day2 = 1, year = 1;
-		assertTrue( 0 == Cal.cal(month1, day1, month2, day2, year));
+		assertTrue( 0 == Cal.cal(month1, day1, month2, day2, year));	
+	}
+	
+	@Test
+	public void testP2() {
+		int month1 = 4, month2 = 5, day1 = 1, day2 = 1, year = 1;
+		assertTrue( 0 == Cal.cal(month1, day1, month2, day2, year));	
 	}
 
 }
