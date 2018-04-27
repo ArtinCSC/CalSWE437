7b2b836cd0241eaf6d2c01874989a0e8751eb56b
Vincent Liu <liuvincent19915@gmail.com>
2018-04-26 21:03:37 -0400

    Added test2PaPbFalse()

--- CACCtest.java
+++ CACCtest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.testng.annotations.Test;
 
 public class CACCtest {
 
@@ -25,6 +26,12 @@ public class CACCtest {
 	public void testP2() {
 		int month1 = 4, month2 = 5, day1 = 1, day2 = 1, year = 1;
 		assertTrue( 0 == Cal.cal(month1, day1, month2, day2, year));	
+	}
+
+	@Test
+	public void test2PaPbFalse(){
+		year = 2004;
+		assertTrue( 31 == Cal.cal(month1, day1, month2, day2, year));
 	}
 
 }
