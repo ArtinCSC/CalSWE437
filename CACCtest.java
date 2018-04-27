e1bfde8bfa9dfcba324d2dcc4f7f9fc7b1885119
Artin <am25938@email.vccs.edu>
2018-04-26 20:51:46 -0400

    k

--- CACCtest.java
+++ CACCtest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class CACCtest {
 
-		private int month1, int day1, int month2, int day2, int year;
+	private int month1, day1, month2, day2, year;
 
 	@Before
 	public void set(){
@@ -18,14 +18,15 @@ public class CACCtest {
 	
 	@Test
 	public void testP1() {
-		int month1 = 4, month2 = 4, day1 = 1, day2 = 1, year = 1;
+		 month1 = 4;
+		 month2 = 4;
 		assertTrue( 0 == Cal.cal(month1, day1, month2, day2, year));	
 	}
 	
 	@Test
 	public void testP2() {
-		int month1 = 4, month2 = 5, day1 = 1, day2 = 1, year = 1;
-		assertTrue( 0 == Cal.cal(month1, day1, month2, day2, year));	
+		 month1 = 4;
+		 month2 = 5;
+		assertTrue( 30 == Cal.cal(month1, day1, month2, day2, year));	
 	}
-
 }
