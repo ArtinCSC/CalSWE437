e33ca9b0672850f9a74248161c59384d8f705c11
Vincent Liu <liuvincent19915@gmail.com>
2018-04-26 20:46:17 -0400

    Implmented before

--- CACCtest.java
+++ CACCtest.java
@@ -6,8 +6,13 @@ import org.junit.Test;
 public class CACCtest {
 
 	@Before
+	private int month1, int day1, int month2, int day2, int year;
 	public void set(){
-		
+		month1 = 1;
+		day1 = 1;
+		month2 = 2;
+		day2 = 1;
+		year = 1;
 	}
 	
 	@Test
